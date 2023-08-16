518442c7cf4e57b6048e8c3e36c39ba8c026e415
DESKTOP-PN7AELV\qhdata <lwf@qhdata.cn>
2023-08-16 16:46:37 +0800

    feat: test eslint

---
+++ packages/helpers/src/index.js
@@ -0,0 +1,12 @@
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp
+  recursionMergeSort()
+}
+
+export function recursionMergeSort() {
+  return
+  const a = 1;
+}
+
